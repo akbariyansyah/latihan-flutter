8c52636621358259bbdeeb6c87bce9e1e44ecfc0
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 18:54:32 +0700

    -Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,47 @@
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Latihan "),),
-        body: Container(),
+        backgroundColor: Colors.green,
+        appBar: AppBar(
+          title: Text("Latihan Card Widget"),
+          flexibleSpace: Container(
+            color: Colors.green,
+          ),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(10),
+          child: ListView(
+            children: [
+              buildCard(Icons.account_balance_wallet, "Balance"),
+              buildCard(Icons.add, "Top Up"),
+              buildCard(Icons.send, "Transfer"),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Card buildCard(IconData iconData, String title) {
+    return Card(
+      elevation: 5,
+      child: Row(
+        children: [
+          Icon(
+            iconData,
+            color: Colors.green,
+          ),
+          Text(title)
+        ],
       ),
     );
   }
