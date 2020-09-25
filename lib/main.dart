dbf6c8adc91eefac858f58d6eb69cc414d89c89d
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 19:34:16 +0700

    -initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,42 @@
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController textController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: Text("Latihan "),),
-        body: Container(),
+        appBar: AppBar(
+          title: Text("Latihan Text Field"),
+        ),
+        body: Container(
+          margin: EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              TextField(
+                maxLength: 8,
+                obscureText: true,
+                controller: textController,
+                onChanged: (value) {
+                  setState(() {});
+                },
+              ),
+              Text(textController.text),
+            ],
+          ),
+        ),
       ),
     );
   }
