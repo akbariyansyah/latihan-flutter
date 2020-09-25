489d77dc4b228299ed18069af65db12eab44e98c
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 18:28:23 +0700

    -complete the code

--- lib/main.dart
+++ lib/main.dart
@@ -7,67 +7,14 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Color color1 = Colors.red;
-  Color color2 = Colors.black26;
-  bool isAccepted = false;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("Latihan Stack dan Alignment"),
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Draggable<Color>(
-                    data: color1,
-                    child: SizedBox(
-                      width: 80,
-                      height: 80,
-                      child: Material(
-                        color: color1,
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                    childWhenDragging: SizedBox(
-                      width: 80,
-                      height: 80,
-                      child: Material(
-                        color: color2,
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                    feedback: SizedBox(
-                      width: 80,
-                      height: 80,
-                      child: Material(
-                        color: color1.withOpacity(0.5),
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                  ),
-                  DragTarget<Color>(
-                    onWillAccept: (value) => true,
-                    onAccept: (value) => isAccepted = true,
-                  )
-                ],
-              )
-            ],
-          )),
+        appBar: AppBar(title: Text("Latihan "),),
+        body: Container(),
+      ),
     );
   }
 }
