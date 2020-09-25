0bcb06a2a13596243a3d5ddb875508e9563b8e8a
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 16:34:42 +0700

    -initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,127 +1,16 @@
-import 'dart:math';
-import 'dart:ui';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Color color1 = Colors.red;
-  Color color2 = Colors.amber;
-  Color color3 = Colors.black12;
-  Color targetColor;
-  bool isAccepted = false;
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text("Latihan Stack dan Alignment"),
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Draggable<Color>(
-                    data: color1,
-                    child: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Material(
-                        color: color1,
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                    childWhenDragging: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Material(
-                        color: color3,
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                    feedback: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Material(
-                        color: color1.withOpacity(0.5),
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                  ),
-                  Draggable<Color>(
-                    data: color2,
-                    child: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Material(
-                        color: color2,
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                    childWhenDragging: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Material(
-                        color: color3,
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                    feedback: SizedBox(
-                      width: 50,
-                      height: 50,
-                      child: Material(
-                        color: color2.withOpacity(0.5),
-                        shape: StadiumBorder(),
-                        elevation: 3.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              DragTarget<Color>(
-                onWillAccept: (value) => true,
-                onAccept: (value) {
-                  isAccepted = true;
-                  targetColor = value;
-                },
-                builder: (context, candidate, rejected) {
-                  return (isAccepted)
-                      ? SizedBox(
-                          width: 100,
-                          height: 100,
-                          child: Material(
-                            color: targetColor,
-                            shape: StadiumBorder(),
-                          ),
-                        )
-                      : SizedBox(
-                          width: 100,
-                          height: 100,
-                          child: Material(
-                            color: color3,
-                            shape: StadiumBorder(),
-                          ),
-                        );
-                },
-              )
-            ],
-          )),
+      home: LoginPage(),
     );
   }
 }
