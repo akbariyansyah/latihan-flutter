36cc6d3e61a44c5940e7a62a73b33bf9ac122e86
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 20:00:06 +0700

    -Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   TextEditingController textController =
       TextEditingController();
+  TextEditingController passController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -26,12 +28,40 @@ class _MyAppState extends State<MyApp> {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               TextField(
+                decoration: InputDecoration(
+                  fillColor: Colors.blue[50],
+                  filled: true,
+                  icon: Icon(Icons.text_fields),
+                  prefixIcon: Icon(Icons.perm_identity),
+                  hintText: "username : ",
+                  hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                  prefixStyle: TextStyle(color: Colors.blue,fontWeight: FontWeight.w400),
+                  labelText: "Username",
+                  labelStyle: TextStyle(fontSize: 20,fontStyle: FontStyle.italic),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))
+                ),
                 maxLength: 8,
-                obscureText: true,
                 controller: textController,
                 onChanged: (value) {
                   setState(() {});
                 },
+              ),
+              TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  fillColor: Colors.blue[50],
+                  filled: true,
+                  icon: Icon(Icons.text_fields),
+                  prefixIcon: Icon(Icons.security),
+                  hintText: "password : ",
+                  hintStyle: TextStyle(fontStyle: FontStyle.italic),
+                  prefixStyle: TextStyle(color: Colors.blue,fontWeight: FontWeight.w400),
+                  labelText: "Password",
+                  labelStyle: TextStyle(fontSize: 20,fontStyle: FontStyle.italic),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))
+                ),
+                maxLength: 8,
+                controller: passController,
               ),
               Text(textController.text),
             ],
