081bee981c1f7f45eea35039ae3f9dc07fbf1ff1
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 21:41:03 +0700

    -Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -9,55 +9,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Latihan MediaQuery"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Button Gradation"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              RaisedButton(
+                color: Colors.amber,
+                onPressed: () {},
+                shape: StadiumBorder(),
+                child: Text("Raised Button"),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: )
+                ),
+                child: Material(
+                  child: InkWell(
+                    child: Text("Custom Button"),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
-      body: (MediaQuery.of(context).orientation == Orientation.portrait)
-          ? Column(children: [
-              Container(
-                color: Colors.red,
-                width: MediaQuery.of(context).size.width / 5,
-                height: MediaQuery.of(context).size.width / 5,
-              ),
-              Container(
-                color: Colors.green,
-                width: MediaQuery.of(context).size.width / 5,
-                height: MediaQuery.of(context).size.width / 5,
-              ),
-              Container(
-                color: Colors.blue,
-                width: MediaQuery.of(context).size.width / 5,
-                height: MediaQuery.of(context).size.width / 5,
-              ),
-            ])
-          : Row(
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: MediaQuery.of(context).size.width / 5,
-                  height: MediaQuery.of(context).size.width / 5,
-                ),
-                Container(
-                  color: Colors.green,
-                  width: MediaQuery.of(context).size.width / 5,
-                  height: MediaQuery.of(context).size.width / 5,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: MediaQuery.of(context).size.width / 5,
-                  height: MediaQuery.of(context).size.width / 5,
-                ),
-              ],
-            ),
     );
   }
 }
