4db77f1b99b6284353ff8cbb10d6d133994bfbe0
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 21:23:34 +0700

    -Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,69 +5,59 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  TextEditingController textController =
-      TextEditingController();
-  TextEditingController passController =
-      TextEditingController();
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Text Field"),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  fillColor: Colors.blue[50],
-                  filled: true,
-                  icon: Icon(Icons.text_fields),
-                  prefixIcon: Icon(Icons.perm_identity),
-                  hintText: "username : ",
-                  hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                  prefixStyle: TextStyle(color: Colors.blue,fontWeight: FontWeight.w400),
-                  labelText: "Username",
-                  labelStyle: TextStyle(fontSize: 20,fontStyle: FontStyle.italic),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))
-                ),
-                maxLength: 8,
-                controller: textController,
-                onChanged: (value) {
-                  setState(() {});
-                },
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  fillColor: Colors.blue[50],
-                  filled: true,
-                  icon: Icon(Icons.text_fields),
-                  prefixIcon: Icon(Icons.security),
-                  hintText: "password : ",
-                  hintStyle: TextStyle(fontStyle: FontStyle.italic),
-                  prefixStyle: TextStyle(color: Colors.blue,fontWeight: FontWeight.w400),
-                  labelText: "Password",
-                  labelStyle: TextStyle(fontSize: 20,fontStyle: FontStyle.italic),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(15))
-                ),
-                maxLength: 8,
-                controller: passController,
-              ),
-              Text(textController.text),
-            ],
-          ),
-        ),
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan MediaQuery"),
       ),
+      body: (MediaQuery.of(context).orientation == Orientation.portrait)
+          ? Column(children: [
+              Container(
+                color: Colors.red,
+                width: MediaQuery.of(context).size.width / 5,
+                height: MediaQuery.of(context).size.width / 5,
+              ),
+              Container(
+                color: Colors.green,
+                width: MediaQuery.of(context).size.width / 5,
+                height: MediaQuery.of(context).size.width / 5,
+              ),
+              Container(
+                color: Colors.blue,
+                width: MediaQuery.of(context).size.width / 5,
+                height: MediaQuery.of(context).size.width / 5,
+              ),
+            ])
+          : Row(
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: MediaQuery.of(context).size.width / 5,
+                  height: MediaQuery.of(context).size.width / 5,
+                ),
+                Container(
+                  color: Colors.green,
+                  width: MediaQuery.of(context).size.width / 5,
+                  height: MediaQuery.of(context).size.width / 5,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: MediaQuery.of(context).size.width / 5,
+                  height: MediaQuery.of(context).size.width / 5,
+                ),
+              ],
+            ),
     );
   }
 }
