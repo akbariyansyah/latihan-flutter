f98f5809baf4c789a204df3e96767751fcc73454
akbariyansyah <riyansean21366@gmail.com>
2020-09-25 17:55:13 +0700

    -Initial Commit -used HexColor package

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/login_page.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  Color color1 = Hexcolor("#333366");
+  Color color2 = Hexcolor("#40e0d0 ");
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: LoginPage(),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.adb,
+            color: Colors.white,
+          ),
+          title: Text(
+            "Latihan Appbar",
+            style: TextStyle(color: Colors.white),
+          ),
+          actions: [
+            IconButton(icon: Icon(Icons.settings), onPressed: () {}),
+            IconButton(icon: Icon(Icons.exit_to_app), onPressed: () {}),
+          ],
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [color1, color2],
+                    begin: FractionalOffset.topLeft,
+                    end: FractionalOffset.bottomRight),
+                image: DecorationImage(
+                    image: AssetImage("assets/pattern.png"),
+                    fit: BoxFit.cover,
+                    repeat: ImageRepeat.repeatX)),
+          ),
+        ),
+      ),
     );
   }
 }
