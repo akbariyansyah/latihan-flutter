c3235840528b4fd492b9d8315996f631b20f4640
akbariyansyah <riyansean21366@gmail.com>
2020-09-27 16:22:15 +0700

    -Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,47 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-class MainPage extends StatelessWidget {
+class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: Center(
+        child: Text("This is working"),
+      ),
+    );
+  }
+}
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
         appBar: AppBar(
-          title: Text("Latihan "),
+          title: Text("Latihan TabBar"),
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.comment),
+                text: "Comments",
+              ),
+              Tab(child: Image(image: AssetImage("assets/cute.png"))),
+              Tab(
+                icon: Icon(Icons.computer),
+              ),
+              Tab(
+                text: "News",
+              ),
+            ],
+          ),
         ),
-        body: Container());
+        body: TabBarView(children: [
+          Center(child: Text("Tab pertama")),
+          Center(child: Text("Tab kedua")),
+          Center(child: Text("Tab ketiga")),
+          Center(child: Text("Tab keempat")),
+        ]),
+      ),
+    );
   }
 }
