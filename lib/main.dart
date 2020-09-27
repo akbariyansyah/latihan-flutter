7c311acc5ba3cf9560f0f72f5e9e790873d199c9
akbariyansyah <riyansean21366@gmail.com>
2020-09-27 19:26:16 +0700

    -Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,8 +20,18 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Latihan "),
+          title: Text("Latihan QR CODE"),
         ),
-        body: Container());
+        body: Center(
+          child: QrImage(
+            version: 6,
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            errorCorrectionLevel: QrErrorCorrectLevel.M,
+            padding: EdgeInsets.all(20),
+            size: 200.00,
+            data: "riyan",
+          ),
+        ));
   }
 }
