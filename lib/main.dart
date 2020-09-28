1868923008be30abc03cdff0c49097d0cf8d4863
akbariyansyah <riyansean21366@gmail.com>
2020-09-28 14:47:35 +0700

    -Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,8 +19,23 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Latihan "),
+          title: Text("Latihan Gradient Opacity"),
         ),
-        body: Container());
+        body: Center(
+          child: ShaderMask(
+              shaderCallback: (rectangle) {
+                return LinearGradient(
+                        colors: [Colors.black, Colors.transparent],
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter)
+                    .createShader(
+                        Rect.fromLTRB(0, 0, rectangle.width, rectangle.height));
+              },
+              blendMode: BlendMode.dstIn,
+              child: Image(
+                  width: 300,
+                  image: NetworkImage(
+                      "https://images.unsplash.com/photo-1593642702749-b7d2a804fbcf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"))),
+        ));
   }
 }
