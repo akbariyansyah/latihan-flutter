69e81d44cbf3bba2baed0012463fda3ca7f99a74
akbariyansyah <riyansean21366@gmail.com>
2020-09-29 21:38:19 +0700

    -Initial Commit -Complete the code

--- lib/main.dart
+++ lib/main.dart
@@ -16,38 +16,49 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(
-            "Latihan Text Style",
+            "Latihan Anonymouse method",
           ),
         ),
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              margin: EdgeInsets.all(10),
+            Center(
               child: Text(
-                "a brown fox jumps over the lazy dog",
-                maxLines: 2,
-                style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                    fontFamily: "Lobster"),
-                textAlign: TextAlign.center,
+                counter.toString(),
+                style: TextStyle(fontSize: 30),
               ),
             ),
-            Container(
-              margin: EdgeInsets.all(10),
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras laoreet iaculis massa, ut maximus risus porttitor sit amet. Phasellus a orci enim. Sed et ultricies libero. Curabitur scelerisque, orci ornare iaculis dignissim, dui nibh imperdiet velit, et tristique quam mi lacinia ligula. Nam eget efficitur neque, id lobortis diam. Fusce a dignissim orci, ac finibus orci. Nullam volutpat quam quis sapien finibus consequat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum porttitor, neque cursus aliquet dictum, nunc neque laoreet dolor, a pharetra ante erat sed ante. Cras sodales quam et lorem posuere, id ultrices turpis elementum. Mauris iaculis nunc venenatis convallis porta. Cras ligula ipsum, hendrerit ac enim non, rutrum accumsan ligula. Sed nec velit at ipsum finibus faucibus. Aliquam cursus nibh a enim venenatis sollicitudin. Duis sed facilisis tortor. Mauris eget nulla mollis, sodales erat quis, pharetra veli",
-                style: TextStyle(fontSize: 15),
-                textAlign: TextAlign.justify,
+            Center(
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    counter += 1;
+                  });
+                },
+                child: Text("PLUS"),
               ),
-            )
+            ),     Center(
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    counter -= 1;
+                  });
+                },
+                child: Text("MINUS"),
+              ),
+            ),
           ],
         ));
   }
