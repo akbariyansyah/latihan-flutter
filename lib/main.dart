d7ec78ac524a8c1c87667b7356188492e77e5b43
akbariyansyah <riyansean21366@gmail.com>
2020-09-29 15:38:26 +0700

    -Initial Commit -Complete the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:myapp/post_result_model.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,41 +16,82 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController jobController = TextEditingController();
+  PostResult postResult = null;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Latihan ClipPath"),
-      ),
-      body: Container(
-        color: Colors.yellow,
-        child: Center(
-          child: ClipPath(
-            clipper: MyClipper(),
-            child: Image(
-              width: 300,
-              image: AssetImage(
-                  "assets/image-3.jpg"),
+        appBar: AppBar(
+          title: Text("Latihan Http Post"),
+        ),
+        body: Container(
+          margin: EdgeInsets.fromLTRB(10, 30, 10, 0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                  child: Text(
+                    "Post New User",
+                    style: TextStyle(
+                        fontSize: 30,
+                        fontFamily: "Lobster",
+                        fontWeight: FontWeight.w500,
+                        color: Colors.black),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "Input name"),
+                    controller: nameController,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20),
+                  child: TextField(
+                    decoration: InputDecoration(hintText: "Input job"),
+                    controller: jobController,
+                  ),
+                ),
+                RaisedButton(
+                  child: Text("POST"),
+                  onPressed: () {
+                    PostResult.PostReq(nameController.text, jobController.text)
+                        .then((res) {
+                      postResult = res;
+                      setState(() {});
+                    });
+                  },
+                ),
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
+                  child: Text(
+                    (postResult != null)
+                        ? "id : " + postResult.id +
+                            "\n" +
+                           "name : " + postResult.name +
+                            "\n" +
+                          "job : " +  postResult.job +
+                            "\n" +
+                           "created at : " + postResult.created
+                        : "Tidak ada data",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
-}
-class MyClipper extends CustomClipper<Path>{
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-    path.lineTo(0, size.height);
-    path.quadraticBezierTo(size.width / 2, size.height * 0.75, size.width, size.height);
-    path.lineTo(size.width, 0);
-    path.close();
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
-  
 }
