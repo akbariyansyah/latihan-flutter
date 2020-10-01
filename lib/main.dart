c811b8b3ab8c12e1b0e0a50674aeadf57338e751
akbariyansyah <riyansean21366@gmail.com>
2020-10-01 11:34:50 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/login_page.dart';
-import 'package:myapp/main_page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:myapp/application_color.dart';
+import 'package:provider/provider.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String name;
-  _MyAppState() {
-    getName().then((value) {
-      setState(() {
-        name = value;
-      });
-    });
-  }
-  Future<String> getName() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    print(pref.getString("username"));
-    return pref.getString("username") ?? "no name inserted";
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: (name == null || name == "no name inserted")
-            ? LoginPage()
-            : MainPage(name: name));
+      home: ChangeNotifierProvider<ApplicationColor>(
+        builder: (context) => ApplicationColor(),
+        child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Consumer<ApplicationColor>(
+              builder: (context,applicationColor,_) => Text(
+                "Latihan provider state management",
+                style: TextStyle(color: applicationColor.color),
+              ),
+            ),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Consumer<ApplicationColor>(
+                  builder: (context,applicationColor,_) => AnimatedContainer(
+                    duration: Duration(milliseconds: 500),
+                    margin: EdgeInsets.all(5),
+                    width: 200,
+                    height: 200,
+                    color: applicationColor.color,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.all(5),
+                      child: Text("LB"),
+                    ),
+                    Consumer<ApplicationColor>(
+                      builder: (context,applicationColor,_) => Switch(
+                        inactiveThumbColor: Colors.amber,
+                          value: applicationColor.isLightBlue,
+                          activeColor: Colors.lightBlue,
+                          onChanged: (newValue) {
+                            applicationColor.isLightBlue = newValue;
+                          }),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(5),
+                      child: Text("LB"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
