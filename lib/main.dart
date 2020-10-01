fa77ba0530f5d946b13c8f9bf001f74a12eda2f7
akbariyansyah <riyansean21366@gmail.com>
2020-10-01 23:43:20 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,63 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/color_bloc.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ColorBloc colorBloc = ColorBloc();
+
+  @override
+  void dispose() {
+    colorBloc.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MainPage(),
-    );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  bool isOn = false;
-  Widget myWidget = Container(
-    width: 200,
-    height: 100,
-    decoration: BoxDecoration(
-        color: Colors.deepOrangeAccent,
-        borderRadius: BorderRadius.circular(20),
-        border: Border.all(color: Colors.pink, width: 3)),
-  );
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          title: Text(
-            "Latihan Switcher ",
-          ),
+          title: Text("Latihan Bloc tanpa package"),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              AnimatedSwitcher(
-                child: myWidget,
-                duration: Duration(milliseconds: 500),
-                transitionBuilder: (child, animation) => RotationTransition(
-                  turns: animation,
-                  child: child,
-                ),
-              ),
-              Switch(
-                  activeColor: Colors.cyan[800],
-                  inactiveThumbColor: Colors.red,
-                  inactiveTrackColor: Colors.red[200],
-                  value: isOn,
-                  onChanged: (newValue) {
-                    isOn = newValue;
-                    setState(() {
-                      if (isOn) {
-                        myWidget = SizedBox(
-                          height: 100,
-                          width: 200,
-                          child: Center(
-                            // child: Text(
-                            //   "Switcher is ON",
-                            //   style: TextStyle(
-                            //     color: Colors.black,
-                            //     fontSize: 30,
-                            //     fontWeight: FontWeight.bold,
-                            //     fontFamily: "Lobster",
-                            //   ),
-                            // ),
-                             child :  Container(
-                               key: ValueKey(1),
-                                width: 200,
-                                height: 100,
-                                decoration: BoxDecoration(
-                                    color: Colors.lightBlue,
-                                    borderRadius: BorderRadius.circular(20),
-                                    border: Border.all(color: Colors.pink, width: 3)),
-                              ),
-                          ),
-                        );
-                      } else {
-                        myWidget = Container(
-                          key: ValueKey(2),
-                          width: 200,
-                          height: 100,
-                          decoration: BoxDecoration(
-                              color: Colors.deepOrange,
-                              borderRadius: BorderRadius.circular(20),
-                              border: Border.all(color: Colors.lightBlue, width: 3)),
-                        );
-                      }
-                    });
-                  })
-            ],
+            child: StreamBuilder(
+          stream: colorBloc.stateStream,
+          initialData: Colors.amber,
+          builder: (context, snapshot) => AnimatedContainer(
+            width: 200,
+            height: 200,
+            color: snapshot.data,
+            duration: Duration(milliseconds: 500),
           ),
-        ));
+        )),
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                colorBloc.eventSink.add(ColorEvent.to_amber);
+              },
+              backgroundColor: Colors.amber,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                colorBloc.eventSink.add(ColorEvent.to_lightblue);
+              },
+              backgroundColor: Colors.lightBlue,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
