3b0dcb3873f965a6c4651d35af4eb7fbb24c22bb
akbariyansyah <riyansean21366@gmail.com>
2020-10-02 14:54:13 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:myapp/color_bloc.dart';
+import 'dart:async';
 
 void main() {
   runApp(MyApp());
@@ -12,52 +11,81 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  ColorBloc colorBloc = ColorBloc();
-
-  @override
-  void dispose() {
-    colorBloc.dispose();
-  }
-
+  bool isStop = true;
+  bool isBlack = true;
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan Bloc tanpa package"),
-        ),
-        body: Center(
-            child: StreamBuilder(
-          stream: colorBloc.stateStream,
-          initialData: Colors.amber,
-          builder: (context, snapshot) => AnimatedContainer(
-            width: 200,
-            height: 200,
-            color: snapshot.data,
-            duration: Duration(milliseconds: 500),
+          appBar: AppBar(
+            backgroundColor: (isBlack) ? Colors.black : Colors.red,
+            title: Text("Latihan Timer"),
           ),
-        )),
-        floatingActionButton: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            FloatingActionButton(
-              onPressed: () {
-                colorBloc.eventSink.add(ColorEvent.to_amber);
-              },
-              backgroundColor: Colors.amber,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  counter.toString(),
+                  style: TextStyle(
+                      fontSize: 50,
+                      color: (isBlack) ? Colors.black : Colors.red),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("Ubah warna 3 detik kemudian"),
+                  onPressed: () {
+                    Timer(Duration(seconds: 3), () {
+                      setState(() {
+                        isBlack = !isBlack;
+                      });
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("Ubah warna secara langsung"),
+                  onPressed: () {
+                    Timer.run(() {
+                      setState(() {
+                        isBlack = !isBlack;
+                      });
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("Start timer"),
+                  onPressed: () {
+                    counter = 0;
+                    isStop = false;
+                    Timer.periodic(Duration(milliseconds: 1), (timer) {
+                      if (isStop) timer.cancel();
+                      setState(() {
+                        counter++;
+                      });
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                RaisedButton(
+                  child: Text("Stop timer"),
+                  onPressed: () {
+                    isStop = true;
+                  },
+                ),
+              ],
             ),
-            SizedBox(
-              width: 10,
-            ),
-            FloatingActionButton(
-              onPressed: () {
-                colorBloc.eventSink.add(ColorEvent.to_lightblue);
-              },
-              backgroundColor: Colors.lightBlue,
-            ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
