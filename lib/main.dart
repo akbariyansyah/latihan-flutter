31e14d9fc835f970aae0431de613d5dfe8a57a56
akbariyansyah <riyansean21366@gmail.com>
2020-10-03 23:08:48 +0700

    -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: BlocProvider<ProductBloc>(
-          create: (context) => ProductBloc(),
+          builder: (context) => ProductBloc(),
           child: MainPage()),
     );
   }
