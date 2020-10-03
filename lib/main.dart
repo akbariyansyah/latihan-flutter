1241ba3b405150842d3ff7bbab709709f68b3e23
akbariyansyah <riyansean21366@gmail.com>
2020-10-03 11:20:37 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.orange,
           title: Text("Latihan Product Card"),
         ),
         body: ChangeNotifierProvider<ProductState>(
