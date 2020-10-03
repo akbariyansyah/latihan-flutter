647a4c5521a9d21f0c7abc5fcedd7ac30c75a59d
akbariyansyah <riyansean21366@gmail.com>
2020-10-03 22:33:29 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'dart:async';
-import 'package:provider/provider.dart';
 import 'package:myapp/product_card.dart';
+import 'package:bloc/bloc.dart';
 void main() {
   runApp(MyApp());
 }
@@ -10,38 +12,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.orange,
-          title: Text("Latihan Product Card"),
-        ),
-        body: ChangeNotifierProvider<ProductState>(
-          builder: (context) => ProductState(),
-          child: Container(
-            margin: EdgeInsets.all(30),
-            child: Align(
-              alignment: Alignment.topCenter,
-              child: Consumer<ProductState>(
-                builder: (context,productState,_) => ProductCard(
-                  imageURL:
-                      "https://images.unsplash.com/photo-1546548970-71785318a17b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-                  name: "Fruits",
-                  price: "Rp. 10.000",
-                  quantity: productState.quantity,
-                  notification: (productState.quantity > 5) ? "Diskon 10 %" : null,
-                  onAddTap: () {
-                    productState.quantity++;
-                  },
-                  onDecTap: () {
-                    productState.quantity--;
-                  },
-                ),
-              ),
+      home: BlocProvider<ProductBloc>(
+          create: (context) => ProductBloc(),
+          child: MainPage()),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  Random random = Random();
+  @override
+  Widget build(BuildContext context) {
+    ProductBloc bloc = BlocProvider.of<ProductBloc>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text("Latihan List view builder")),
+      body: Column(
+        children: [
+          RaisedButton(onPressed: () {
+            bloc.dispatch(random.nextInt(6));
+          }),
+          BlocBuilder<ProductBloc,List<Product>>(
+            builder: (context,products) => Expanded(
+              child: ListView.builder(
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    return ProductCard(imageURL: products[index].imageURL,
+                    price: products[index].price.toString(),name: products[index].name,
+                    onDecTap: () {},
+                    onAddTap: () {},
+                    notification: "",);
+                  }),
             ),
-          ),
-        ),
+          )
+        ],
       ),
     );
+  }
+}
+
+
+class Product {
+  String imageURL;
+  String name;
+  int price;
+  Product({this.imageURL="",this.price=0,this.name=""});
+}
+
+class ProductBloc extends Bloc<int,List<Product>> {
+
+  @override
+  List<Product> get initialState => [];
+  
+  @override
+  Stream<List<Product>> mapEventToState(int event) async* {
+    List<Product> products = [];
+    for (int i=0;i<event;i++) {
+      products.add(Product(name: "Frutis",imageURL: "https://images.unsplash.com/photo-1511688878353-3a2f5be94cd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80",
+      price: (i + i) * 2000));
+    }
+    yield products;
   }
 }
 class ProductState extends ChangeNotifier {
