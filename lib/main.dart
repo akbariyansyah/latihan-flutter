128d2aa5dd2b2e2054a02ca586dc4e4a50bd05b7
akbariyansyah <riyansean21366@gmail.com>
2020-10-04 22:01:24 +0700

    -initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,67 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:bloc/bloc.dart';
-import 'package:myapp/bloc/user_bloc.dart';
-import 'package:myapp/ui/main_page.dart';
 
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Person selectedPerson;
+
+  List<Person> persons = [Person("Dani"), Person("Joko")];
+
+  List<DropdownMenuItem> generateItems(List<Person> persons) {
+    List<DropdownMenuItem> items = [];
+    for (var person in persons) {
+      items.add(
+        DropdownMenuItem(
+          child: Text(person.name),
+          value: person,
+        ),
+      );
+    }
+    return items;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BlocProvider(builder: (context) => UserBloc(),
-      child: MainPage(),),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Latihan drop down button "),
+          ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(20),
+                child: DropdownButton(
+                    isExpanded: true,
+                    style: TextStyle(fontSize: 20,color: Colors.black),
+                    items: generateItems(persons),
+                    value: selectedPerson,
+                    onChanged: (item) {
+                      setState(() {
+                        selectedPerson = item;
+                      });
+                    }),
+              ),
+              SizedBox(height: 20,),
+              Text((selectedPerson != null) ? selectedPerson.name : "Belum ada yang terpilih",style: TextStyle(fontSize: 30),),
+            ],
+          ),
+        ),
+      ),
     );
   }
+}
+
+class Person {
+  String name;
+  Person(this.name);
 }
