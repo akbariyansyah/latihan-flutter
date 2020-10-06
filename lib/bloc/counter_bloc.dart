e354149f40666afbe0a6e1e008f683167cf632c2
akbariyansyah <riyansean21366@gmail.com>
2020-10-06 21:48:17 +0700

    Initial Commit,Complete the code

--- lib/bloc/counter_bloc.dart
+++ lib/bloc/counter_bloc.dart
@@ -1,13 +1,21 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
 
+part 'counter_event.dart';
+part 'counter_state.dart';
 
-class CounterBloc extends Bloc<int,int> {
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
   @override
-  int get initialState => 0;
+  CounterState get initialState => CounterState(0);
 
   @override
-  Stream<int> mapEventToState(int event) async* {
-    yield event;
+  Stream<CounterState> mapEventToState(
+    CounterEvent event,
+  ) async* {
+    yield (event is Increment)
+        ? CounterState(state.counter + 1)
+        : CounterState(state.counter - 1);
   }
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,65 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc/bloc.dart';
-import 'package:myapp/bloc/color_bloc.dart';
 import 'package:myapp/bloc/counter_bloc.dart';
-import 'package:myapp/ui/main_page.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<ColorBloc>(builder: (context) => ColorBloc(),),
-        BlocProvider<CounterBloc>(builder: (context) => CounterBloc(),),
-      ],
-      child: MaterialApp(
-        home: MainPage(),
+    return MaterialApp(
+      home: BlocProvider<CounterBloc>(
+          builder: (context) => CounterBloc(), child: MainPage()),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    CounterBloc bloc = BlocProvider.of<CounterBloc>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan Bloc ver 1.0"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<CounterBloc,CounterState>(
+              builder: (context, counterState) => Text(
+                counterState.counter.toString(),
+                style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    bloc.add(Decrement());
+                  },
+                  child: Icon(Icons.arrow_downward),
+                ),
+                SizedBox(
+                  width: 40,
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    bloc.add(Increment());
+                  },
+                  child: Icon(Icons.arrow_upward),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
