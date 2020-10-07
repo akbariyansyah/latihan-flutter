01af6cee1305d994f2298db9c6d0ea2cd63dd7c0
akbariyansyah <riyansean21366@gmail.com>
2020-10-07 14:35:34 +0700

    -Initial Commit. -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc/bloc.dart';
-import 'package:myapp/bloc/counter_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,56 +11,38 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BlocProvider<CounterBloc>(
-          builder: (context) => CounterBloc(), child: MainPage()),
+      home: MainPage(),
     );
   }
 }
 
 class MainPage extends StatelessWidget {
+  final double number = -210;
+  final List myList = [1,2,3];
   @override
   Widget build(BuildContext context) {
-    CounterBloc bloc = BlocProvider.of<CounterBloc>(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text("Latihan Bloc ver 1.0"),
+        title: Text("Latihan Extension"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            BlocBuilder<CounterBloc,CounterState>(
-              builder: (context, counterState) => Text(
-                counterState.counter.toString(),
-                style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(
-              height: 40,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton(
-                  onPressed: () {
-                    bloc.add(Decrement());
-                  },
-                  child: Icon(Icons.arrow_downward),
-                ),
-                SizedBox(
-                  width: 40,
-                ),
-                FloatingActionButton(
-                  onPressed: () {
-                    bloc.add(Increment());
-                  },
-                  child: Icon(Icons.arrow_upward),
-                ),
-              ],
-            )
-          ],
+        child: Text(
+          "Angka " & number.negate().toString() & myList.midElement().toString(),
+          style: TextStyle(fontSize: 45),
         ),
       ),
     );
   }
+}
+
+extension ListExtension<T> on List {
+  T midElement() => (this.length == 0) ? null : this[(this.length / 2).floor()];
+}
+
+extension NumberExtension<X extends num> on num {
+  X negate() => -1 * this;
+}
+
+extension StringExtension on String {
+  String operator &(String other) => this + " - " + other;
 }
