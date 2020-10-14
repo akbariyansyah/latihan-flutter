dbf5dc7f53d61053127fd97a715d490d273da301
akbariyansyah <riyansean21366@gmail.com>
2020-10-14 22:48:35 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,47 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> with WidgetsBindingObserver {
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.inactive) {
+      print(">>>>>>>>> APP INACTIVE <<<<<<<<<<<");
+    }
+    if (state == AppLifecycleState.paused) {
+      print(">>>>>>>>> APP PAUSED <<<<<<<<<<<");
+    }
+    if (state == AppLifecycleState.resumed) {
+      print(">>>>>>>>> APP RESUMED <<<<<<<<<<<");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Latihan "),
+          title: Text("Latihan Application life cycle"),
         ),
         body: Container());
   }
+
+
 }
