ee33196b91248ceff27c31873e7be8176161478e
akbariyansyah <riyansean21366@gmail.com>
2020-10-15 11:34:31 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -14,13 +15,61 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+  double radius = 0;
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: Duration(seconds: 3), vsync: this);
+    animation = Tween<double>(begin: 0, end: pi * 2).animate(controller)
+      ..addListener(() {
+        setState(() {
+          radius = animation.value;
+        });
+      })..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+    controller.forward();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Latihan "),
+          title: Text("Latihan Animation"),
         ),
-        body: Container());
+        body: Center(
+          child: Transform.rotate(
+            angle: animation.value,
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(radius * 20),
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.red,
+                      Colors.purple,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  )),
+            ),
+          ),
+        ));
   }
 }
