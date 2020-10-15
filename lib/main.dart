2a710bce95b584770eeb10ff7d2a9af2e5292731
akbariyansyah <riyansean21366@gmail.com>
2020-10-15 14:31:33 +0700

    -Initial Commit -Complete the code.

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,8 @@ class _MainPageState extends State<MainPage>
         setState(() {
           radius = animation.value;
         });
-      })..addStatusListener((status) {
+      })
+      ..addStatusListener((status) {
         if (status == AnimationStatus.completed) {
           controller.reverse();
         } else if (status == AnimationStatus.dismissed) {
@@ -46,30 +47,78 @@ class _MainPageState extends State<MainPage>
   }
 
   @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Latihan Animation"),
+          title: Text("Latihan Animated Widget & Animated Builder"),
         ),
         body: Center(
-          child: Transform.rotate(
-            angle: animation.value,
-            child: AnimatedContainer(
-              duration: Duration(seconds: 1),
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(radius * 20),
-                  gradient: LinearGradient(
-                    colors: [
-                      Colors.red,
-                      Colors.purple,
-                    ],
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                  )),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            RotatingContainer(
+              doubleAnimation: animation,
             ),
-          ),
-        ));
+            RotationAnimation(
+              doubleAnimation: animation,
+              child: Container(
+                width: 150,
+                height: 150,
+                color: Colors.purple,
+              ),
+            ),
+            RotationAnimation(
+              doubleAnimation: animation,
+              child: Text(
+                "Hello World",
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+          ],
+        )));
+  }
+}
+
+class RotatingContainer extends AnimatedWidget {
+  RotatingContainer({Key key, Animation<double> doubleAnimation})
+      : super(key: key, listenable: doubleAnimation);
+
+  @override
+  Widget build(BuildContext context) {
+    Animation<double> animation = listenable as Animation<double>;
+    return Transform.rotate(
+      angle: animation.value,
+      child: Container(
+        width: 100,
+        height: 100,
+        decoration: BoxDecoration(color: Colors.red),
+      ),
+    );
+  }
+}
+
+class RotationAnimation extends StatelessWidget {
+  final Animation<double> doubleAnimation;
+  final Widget child;
+  RotationAnimation({this.doubleAnimation, this.child});
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        child: child,
+        animation: doubleAnimation,
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: doubleAnimation.value,
+            child: Container(
+              child: child,
+            ),
+          );
+        });
   }
 }
