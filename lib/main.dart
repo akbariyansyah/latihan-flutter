52b231bc0a615d7ad1dcf4a66c6dd3111f53936b
akbariyansyah <riyansean21366@gmail.com>
2020-10-20 10:57:06 +0700

    -initial commit -complete the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,12 +16,44 @@ class MyApp extends StatelessWidget {
 }
 
 class MainPage extends StatelessWidget {
+  int money = 1000000;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Latihan "),
+          title: Text("Latihan Format Currency "),
         ),
-        body: Container());
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "normal",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              money.toString(),
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(height: 10,),
+             Text(
+              "long currency",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              NumberFormat.currency(locale: "id",symbol: "Rp ",decimalDigits: 0).format(money),
+              style: TextStyle(fontSize: 30),
+            ),
+            SizedBox(height: 10,),
+             Text(
+              "compact currency",
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              NumberFormat.compactCurrency(locale: "id",decimalDigits: 0).format(money + 35000),
+              style: TextStyle(fontSize: 30),
+            ),
+          ],
+        ),),);
   }
 }
